032bd64b3472922e23b96b2a6da87d0cb5d7a7b4
victorseguin <victor.seg2@gmail.com>
2019-01-23 16:54:44 +0100

    get all the franch restaurants url

--- utils.js
+++ utils.js
@@ -1,12 +1,61 @@
-const castle = require('castle');
+//const castle = require('castle');
 
-const restaurant;
+var jquery = require('jquery');
+var cheerio = require('cheerio');
+var request = require('request');
+var url = 'https://www.relaischateaux.com/fr/site-map/etablissements';
+
+var Frenchlist = [{
+    'restaurant': '',
+    'chef': '',
+    'hote': '',
+  }];
+
+request(url, function(err, resp, body) {
+    var $ = cheerio.load(body);
+    
+
+    $("#countryF").each(function(i){
+        console.log('pays : ', $(this).find("h3").text());
+        console.log("iteration - ", i);
+
+        if ($(this).find("h3").text() === 'France'){
+            console.log("ok france");
+
+            $(this).find("li").each(function(j){
+                console.log('restaurant numéro ', j);
+
+                $(this).find("a").each(function(k){
+                    console.log(k);
+                    if(k==0){
+                        let val = $(this).attr("href");
+                        console.log(val);
+
+                        Frenchlist[j].restaurant = val;
+                    }
+                    if(k==1){
+                        console.log($(this).attr("href"));
+                    }
+                    if(k==2){
+                        console.log($(this).attr("href"));
+                    }
+                })
+            })
+        }
+    })
+});
 
 
 
-const hotels = castle.getHotels();
-const prices = castle.getPrices(restaurant);
+/*
+if(k == 0){
+    var elt = $("a:first");
+    console.log(elt);
+}
 
-const michelin = require('michelin');
-
-const starred = michelin.get();
+if(k == 1){
+    Frenchlist[j].chef = $(this)('li').html();
+}
+if(k == 2){
+    Frenchlist[j].Host = $(this)('li').html();
+}*/
