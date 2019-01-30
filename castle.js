88172702e3aaf55c3f3e62c32c4db2b5daa38cbb
victorseguin <victor.seg2@gmail.com>
2019-01-30 11:02:24 +0100

    getting the starred restaurants of all pages in michelin

--- castle.js
+++ castle.js
@@ -1,4 +1,51 @@
-var castleList = {
+var jquery = require('jquery');
+var cheerio = require('cheerio');
+var request = require('request');
+var url = 'https://www.relaischateaux.com/fr/site-map/etablissements';
 
-} ;
+var Frenchlist = [{
+    'restaurant': '',
+    'chef': '',
+    'hote': '',
+    'prix' : ''
+  }];
 
+request(url, function(err, resp, body) {
+    var $ = cheerio.load(body);
+    
+
+    $("#countryF").each(function(i){
+        console.log('pays : ', $(this).find("h3").text());
+        console.log("iteration - ", i);
+
+        if ($(this).find("h3").text() === 'France'){
+            console.log("ok france");
+
+            $(this).find("li").each(function(j){
+
+                console.log('restaurant numéro ', j);
+                //k-ieme restaurant
+                console.log($(this).filter("a").text());
+
+                $(this).find("a").each(function(k){
+
+                    if(k==0){
+                        console.log($(this).filter("a").text());
+                        //nom du restaurant
+
+                        console.log($(this).attr("href"));
+                        //url restaurant
+                    }
+                    if(k==1){
+                        console.log($(this).attr("href"));
+                        //url du chef
+                    }
+                    if(k==2){
+                        console.log($(this).attr("href"));
+                        //url de l'hote
+                    }
+                })
+            })
+        }
+    })
+});
--- michelin.js
+++ michelin.js
@@ -1,4 +1,3 @@
-var jquery = require('jquery');
 var cheerio = require('cheerio');
 var request = require('request');
 
@@ -34,39 +33,38 @@ var listeRestaurants = [{
     'price': ''
 }];
 
-
-  //recuperation du nombre de pages: 35 
+var p1 = new Promise(function(resolve, reject) {
 request(url,function(err,resp,body){
   var $=cheerio.load(body);
-
   var page = -1;
-
-    //on récupère le nom des restaurants d'une page
+  //on récupère le nom des restaurants d'une page
   $(".mr-pager-item").each(function(i){
-
     if(max(page, $(this).text()) == parseInt($(this).text())){
       page = parseInt($(this).text());
     }
     console.log('token retenu : ' + page);
   })
+  resolve(page);
 });
-  
+});
 
+p1.then((page)=>{
 for(var i = 1; i < page + 1; i++){
-
   var urlparpage = url + '/page-'+ i.toString();
+  console.log('page : '+ i);
   console.log(urlparpage);
-
+  
   request(urlparpage,function(err,resp,body){
     var $=cheerio.load(body);
 
     //on récupère le nom des restaurants d'une page
     $(".poi_card-display-title").each(function(i){
-
-      console.log('restaurant numero : ', i + 1);
-      console.log($(this).text(), '\n \n');
-
+      console.log($(this).text(), '\n');
     })
-
-  });
+    });
 }
+});
+
+
+
+  
